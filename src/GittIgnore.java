592b1e2f5ef5c3c506c2a7d63f6989507f25a520
hasan <getdatapro@gmail.com>
2022-08-21 16:59:46 +0300

    conflictV1

--- src/GittIgnore.java
+++ src/GittIgnore.java
@@ -1,5 +1,6 @@
 public class GittIgnore {
     public static void main(String[] args) {
         System.out.println("GittIgnoreDeneme1");
+        System.out.println("GittIgnoreDenemeHasanBranch");
     }
 }
